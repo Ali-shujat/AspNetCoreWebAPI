53790c3de27cff9ae02585032f08d95a2c328ad0
Shujat Ali <shuja4you@hotmail.com>
2020-08-31 15:40:45 +0100

    Add project files.

---
+++ AspNetCoreWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing","Hot","Breezing","Chilly","Mild","Warm","Sweltering"
+        };
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rand = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rand.Next(-20, 55),
+                Summary = Summaries[rand.Next(Summaries.Length)]
+
+            }).ToArray();
+        }
+
+    }
+}
